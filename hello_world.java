d7602becf4003697f049047ff0d6dc92fa3ad974
viv-dan <vivekvardhandanu@gmail.com>
2021-04-03 21:33:46 +0530

    these are also dan's changes

--- hello_world.java
+++ hello_world.java
@@ -2,5 +2,6 @@ public class world_hello{
   public static void main(String[] args){
     System.out.println("hello world");
     System.out.println("cloned successfully");
+	world_hello o=new world_hello;
   }
 }
